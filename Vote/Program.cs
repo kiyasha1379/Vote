64cb77b2adcfa75aa1fe778fdfd81bc15c7d9ce7
kiyasha1379 <amirrezaamelii1379@gmail.com>
2025-09-28 13:20:59 +0330

    change teh token

--- Vote/Program.cs
+++ Vote/Program.cs
@@ -4,7 +4,7 @@
     {
         Console.WriteLine("Bot is starting...");
 
-        var botHandler = new BotHandler("8274908628:AAEHQELuBPYuxvliXMKXYwr4GX0DXAQ9Eck");
+        var botHandler = new BotHandler("8289170032:AAEIcEjl7AZmnk1KmYJlWREVrKKlSpQXgo0");
 
         Console.WriteLine("Bot is running. Press any key to exit.");
         await Task.Delay(-1);
