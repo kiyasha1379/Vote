83075defdfe985e958b90f95d16282df251eaf3e
kiyasha1379 <amirrezaamelii1379@gmail.com>
2025-09-28 13:05:00 +0330

    .

--- Vote/Program.cs
+++ Vote/Program.cs
@@ -1,12 +1,12 @@
 ﻿class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.WriteLine("Bot is starting...");
 
         var botHandler = new BotHandler("8274908628:AAEHQELuBPYuxvliXMKXYwr4GX0DXAQ9Eck");
 
         Console.WriteLine("Bot is running. Press any key to exit.");
-        Console.ReadKey();
+        await Task.Delay(-1);
     }
 }
