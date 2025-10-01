f6aa9233da380f0870ba8544c485c23c07667486
h.khazaee <h.khazaee@aranuma.com>
2025-10-01 17:04:58 +0330

    change token

--- Vote/Program.cs
+++ Vote/Program.cs
@@ -5,7 +5,8 @@
         Console.WriteLine("Bot is starting...");
 
         //var botHandler = new BotHandler("8497081800:AAHKeOdBgdSJCjxyfOv0aCJ35_CrYVkY7VY");
-        var botHandler = new BotHandler("8289170032:AAEIcEjl7AZmnk1KmYJlWREVrKKlSpQXgo0");
+        //var botHandler = new BotHandler("8289170032:AAEIcEjl7AZmnk1KmYJlWREVrKKlSpQXgo0");
+        var botHandler = new BotHandler("8497081800:AAHKeOdBgdSJCjxyfOv0aCJ35_CrYVkY7VY");
 
         Console.WriteLine("Bot is running. Press any key to exit.");
 
